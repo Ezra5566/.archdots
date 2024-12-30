e9c4104379b26fd374fa62222872002d26ddaa4a
ezra odyn <ezraakush@gmail.com>
2024-12-30 08:45:17 +0300

    minor changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,12 +4,16 @@ if status is-interactive
     starship init fish | source
 end
 
+set -x EDITOR "vim" 
+
 # List Directory
 alias l='eza -lh  --icons=auto' # long list
 alias ls='eza -1   --icons=auto' # short list
 alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
 alias ld='eza -lhD --icons=auto' # long list dirs
 alias lt='eza --icons=auto --tree' # list folder as tree
+alias c='code'
+alias zl='lazygit'
 # fzf search
 alias fzf='fzf --height 40% --layout=reverse --border --preview "bat --style=numbers --color=always {}"'
 alias fzfp='fzf --preview "bat --style=numbers --color=always {}"'
