bdd3a1c6c5fce2cee7e185e75de9263cfcea216c
ezra odyn <ezraakush@gmail.com>
2025-01-01 16:42:21 +0300

    added lsd (ill use both lsd /eza)

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,6 +9,7 @@ set -x EDITOR "vim"
 # List Directory
 alias l='eza -lh  --icons=auto' # long list
 alias ls='eza -1   --icons=auto' # short list
+alias lsd='lsd --group-directories-first -A'
 alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
 alias ld='eza -lhD --icons=auto' # long list dirs
 alias lt='eza --icons=auto --tree' # list folder as tree
@@ -27,3 +28,4 @@ abbr .5 'cd ../../../../..'
 # Always mkdir a path (this doesn't inhibit functionality to make a single dir)
 abbr mkdir 'mkdir -p'
 zoxide init fish | source
+atuin init fish | source
