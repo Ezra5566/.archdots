e5ca3cd6847de84b659586d880f476e72937c106
ezra odyn <ezraakush@gmail.com>
2025-01-12 04:29:17 +0300

    added fish code and updated kitty

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,7 +12,7 @@ alias ls='eza -1   --icons=auto' # short list
 alias lsd='lsd --group-directories-first -A'
 alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
 alias ld='eza -lhD --icons=auto' # long list dirs
-alias lt='eza --icons=auto --tree' # list folder as tree
+alias lt='eza --tree --level=1 --color=always --long --git --icons=always --no-time --no-user --no-permissions' # list folder as tree
 alias c='code'
 alias zl='lazygit'
 alias cmatrix='unimatrix -n -s 96 -l o'
@@ -25,7 +25,16 @@ abbr ... 'cd ../..'
 abbr .3 'cd ../../..'
 abbr .4 'cd ../../../..'
 abbr .5 'cd ../../../../..'
-
+#------------------------yazi------------------------
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+#----------------------------------------------------
 # Always mkdir a path (this doesn't inhibit functionality to make a single dir)
 abbr mkdir 'mkdir -p'
 zoxide init fish | source
